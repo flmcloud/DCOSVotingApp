669cb4b85859cf85c5a166a361a9760c6b254479
flmcloud <flmcloud@outlook.com>
2016-04-28 14:54:20 +0100

    edit

--- voting-app/app.py
+++ voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Sun")
+option_b = os.getenv('OPTION_B', "Rain")
 
 hostname = socket.gethostname()
 
